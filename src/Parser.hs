40e25af6c67d001e4a202e257607a648c6d7de3c
Mike Limansky <mike.limansky@gmail.com>
2011-11-14 02:09:56 +0400

    Get public index and public id.

--- src/Parser.hs
+++ src/Parser.hs
@@ -11,19 +11,24 @@ module Parser where
 
 import qualified Data.ByteString.Lazy as L
 import Data.Attoparsec
+import Data.Attoparsec.Binary (anyWord32be)
 import Control.Monad (liftM)
+import Data.Word
 
 data WbxmlVersion = Version1_0 | Version1_1 | Version1_2 | Version1_3
     deriving (Show, Eq, Enum)
 
 data WbxmlDocument = WbxmlDocument {
           documentVersion :: WbxmlVersion
-        , decumentVersionId :: Int
+        , documentPublicId :: Word32
+        , documentPublicIndex :: Word32
     } deriving (Show)
 
 parseDocument = do
     version <- parseVersion
-    return $ WbxmlDocument version 0
+    publicId <- anyWord32be
+    publicIndex <- if publicId == 0 then anyWord32be else return 0
+    return $ WbxmlDocument version publicId publicIndex
 
 parseVersion = liftM (toEnum . fromIntegral) (satisfy supportedVersion
                 <?> "Unsupported version")

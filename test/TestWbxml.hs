26231eba5490a4b09d1ecec7e29254b2cda0305d
Mike Limansky <mike.limansky@gmail.com>
2012-02-11 01:08:52 +0400

    Make conditional compilation for tests.

--- test/TestWbxml.hs
+++ test/TestWbxml.hs
@@ -1,8 +1,13 @@
+{-# LANGUAGE CPP #-}
+
 import Test.Framework (defaultMain, testGroup)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
 import TestSAX
+
+#ifdef WithHaXml
 import TestBindingHaXml
+#endif 
 
 main = defaultMain tests
 
@@ -11,7 +16,9 @@ tests = [ testGroup "SAX parser tests"
             , testProperty "Test multibyte words parser" prop_word32mb
             , testProperty "Test opaque data parser" prop_opaque
             ]
+#ifdef WithHaXml
         , testGroup "HaXML binding tests"
             [ testCase "Test inner elements" tst_inner
             ]
+#endif
         ]
